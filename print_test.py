66faf04571a69faf5f4ff9439f4352b543f0705e
tedesch <ttedeschi3621@floridapoly.edu>
2026-01-25 20:42:57 -0500

    hi

---
+++ print_test.py
@@ -0,0 +1,5 @@
+my_name = "Tal Tedeschi"
+print("Hello, my name is " + my_name)
+print("Use 4 spaces per indentation level.")
+print("Python reads program text as Unicode code points")
+print("I hope to gain strong problem-solving skills, the ability to build real Python programs, and greater academic andcareer opportunities.")
